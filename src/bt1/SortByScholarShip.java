881fa7488abcf775a884e62bc2d5569246ec9873
lemminh54106 <leminh54106@gmail.com>
2024-07-08 17:00:38 +0700

    ss10

---
+++ src/bt1/SortByScholarShip.java
@@ -0,0 +1,12 @@
+package bt1;
+
+import java.util.Comparator;
+
+public class SortByScholarShip implements Comparator<BKapStudent> {
+
+
+    @Override
+    public int compare(BKapStudent o1, BKapStudent o2) {
+        return (int) (o1.getScholarShip()-o2.getScholarShip());
+    }
+}
